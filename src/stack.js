00efb1b4226717c76b374272157255757c2902fc
SeriakovaOksana <hikki0019@gmail.com>
2023-04-17 12:29:29 +0300

    feat: add solution for stack

--- src/stack.js
+++ src/stack.js
@@ -13,19 +13,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.st = [];
+    this.po = null;
+    this.pe= null;
+  }
+
+  push(element) {
+    this.st.push(element);
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.po = this.st.pop();
+    this.st.pop();
+    return this.po;
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.pe = this.st.pop();
+    return this.pe;
   }
 }
 
